bcdc913302899428e8d9104975ddc8d59007ba41
Liao Yi Ting <liaoyitingg@hotmail.com>
2018-11-03 01:17:15 +0800

    - bug fixed

--- app/OleAdmin/src/java/servlet/SpecialsServlet.java
+++ app/OleAdmin/src/java/servlet/SpecialsServlet.java
@@ -46,12 +46,9 @@ public class SpecialsServlet extends HttpServlet {
             specialsList = SpecialsDAO.getAllSpecials();
             if (specialsList != null) {
                 request.setAttribute("specials", specialsList);
-<<<<<<< HEAD
-                if (id != null && !id.equals("")) {
-=======
+
                 System.out.println(id);
-                if (id!= null && !id.equals("")) {
->>>>>>> 6b71b1fe0ee2c3ae5315f44d267045897abdfcc1
+                if (id != null && !id.equals("")) {
                     rd = request.getRequestDispatcher("leagueSpecials.jsp?id="+id);
                     //System.out.println(usersList.size());
                     rd.forward(request, response);
