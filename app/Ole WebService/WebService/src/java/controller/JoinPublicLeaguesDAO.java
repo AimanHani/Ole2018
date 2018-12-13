b39ba769edf3c3f882c6d8703b6a9706dfdc9838
Li Wei En <weien.li.2016@smu.edu.sg>
2018-12-13 16:49:33 +0800

    added web service for JoinPublicLeagues

---
+++ app/Ole WebService/WebService/src/java/controller/JoinPublicLeaguesDAO.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import static controller.SignUpDAO.SHA1;
+import dbConnection.DBConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import model.AllPublicLeague;
+
+/**
+ *
+ * @author user
+ */
+public class JoinPublicLeaguesDAO {
+
+    public static String insertToLog(String username, int leagueID) {
+        int rs = 0;
+        // I follow the php script to set the point as 0 here
+        int points = 0;
+        try (Connection c = DBConnection.getConnection(); PreparedStatement ps = c.prepareStatement("INSERT INTO log (username, leagueId, points) VALUES (?,?,?)");) {
+
+            ps.setString(1, username);
+            ps.setInt(2, leagueID);
+            ps.setInt(3, points);
+            rs = ps.executeUpdate();
+            if (rs > 0) {
+                return "successful";
+            }
+        } catch (Exception e) {
+            System.out.println("check db connection class");
+        }
+        return "error";
+    }
+
+    public static int selectLog(String username, int leagueID) {
+        int logID = 0;
+        try (Connection conn = DBConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement("SELECT logId from log where username = ? and leagueId = ? ");) {
+            stmt.setString(1, username);
+            stmt.setInt(2, leagueID);
+            
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                logID = rs.getInt(1);
+
+            }
+            rs.close();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            System.out.println(ex);
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        return logID;
+
+    }
+
+    public static String insertToMatchLog(int logID) {
+        int points = 0;
+        int rs = 0;
+        try (Connection c = DBConnection.getConnection(); PreparedStatement ps = c.prepareStatement("INSERT INTO matchlog (logId, points) VALUES (?,?)");) {
+            ps.setInt(1, logID);
+            ps.setInt(2, points);
+            rs = ps.executeUpdate();
+            if (rs > 0) {
+                return "successful";
+            }
+        } catch (Exception e) {
+            System.out.println("check db connection class");
+        }
+        return "error";
+
+    }
+
+    public static ArrayList<Integer> getSpecialsIDs() {
+        int points = 0;
+
+        ArrayList<Integer> specialsIDs = new ArrayList();
+        try (Connection c = DBConnection.getConnection(); PreparedStatement ps = c.prepareStatement("SELECT specialsId from specialslog where prediction = -1");) {
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                specialsIDs.add(rs.getInt(1));
+            }
+            rs.close();
+
+        } catch (Exception e) {
+            System.out.println("check db connection class");
+        }
+        return specialsIDs;
+    }
+
+    public static String insertSpecialsLog(int logID) {
+        int points = 0;
+        int rs = 0;
+        ArrayList<Integer> specialsIDs = getSpecialsIDs();
+        for (int i = 0; i < specialsIDs.size(); i++) {
+            try (Connection c = DBConnection.getConnection(); PreparedStatement ps = c.prepareStatement("INSERT INTO specialsLog (logid, specialsId) VALUES(?,?)");) {
+                ps.setInt(1, logID);
+                ps.setInt(2, specialsIDs.get(i));
+                rs = ps.executeUpdate();
+
+            } catch (Exception e) {
+                System.out.println("check db connection class");
+                return "error";
+            }
+        }
+
+        return "successful";
+    }
+}
---
+++ app/Ole WebService/WebService/src/java/json/JoinPublicLeagues.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package json;
+
+import controller.JoinPublicLeaguesDAO;
+import controller.SignUpDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author user
+ */
+@WebServlet(name = "JoinPublicLeaguesJson", urlPatterns = {"/json/joinPublicLeagues"})
+public class JoinPublicLeagues extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet JoinPublicLeagues</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet JoinPublicLeagues at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        JSONObject json = new JSONObject();
+        response.setContentType("\"Content-Type\", \"application/x-www-form-urlencoded\"");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter out = response.getWriter();
+
+        String username = request.getParameter("username");
+        int legaueID = Integer.parseInt(request.getParameter("leagueId"));
+        String status = "";
+        int logID = 0;
+        try {
+
+            status = JoinPublicLeaguesDAO.insertToLog(username, legaueID);
+            if (status.equals("successful")) {
+                logID = JoinPublicLeaguesDAO.selectLog(username, legaueID);
+                status = JoinPublicLeaguesDAO.insertSpecialsLog(logID);
+                if (status.equals("successful")) {
+
+                    json.put("Status", "Public league join successful");
+
+                } else {
+                    json.put("status", "error");
+                    String invalidMsg = "Something is wrong check checkS" + "/" + "";
+                    String[] invalidString = {invalidMsg};
+                    json.put("messages", invalidString);
+                }
+            } else {
+
+                json.put("status", "error");
+                String invalidMsg = "Something is wrong check checkS" + "/" + "";
+                String[] invalidString = {invalidMsg};
+                json.put("messages", invalidString);
+            }
+            out.print(json);
+            out.flush();
+
+        } catch (JSONException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
