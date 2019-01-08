d301f4b8bcf8bb791bdf981797011a41cd5ab094
Li Wei En <weien.li.2016@smu.edu.sg>
2019-01-08 16:23:49 +0800

    edited User profile for Webservice

--- app/Ole WebService/WebService/src/java/controller/PublicLeagueProfileDAO.java
+++ app/Ole WebService/WebService/src/java/controller/PublicLeagueProfileDAO.java
@@ -13,6 +13,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import model.PublicLeagueProfile;
 import java.sql.Time;
+import java.util.ArrayList;
+import model.User;
+import org.eclipse.persistence.jpa.jpql.parser.DateTime;
 
 /**
  *
@@ -20,11 +23,12 @@ import java.sql.Time;
  */
 public class PublicLeagueProfileDAO {
 
-    public static PublicLeagueProfile getUserPrediction(String username) {
+    public static ArrayList<PublicLeagueProfile> getUserPrediction(String username) {
+        ArrayList<PublicLeagueProfile>leagueList = new ArrayList();
         PublicLeagueProfile plProfile = new PublicLeagueProfile();
-        try (Connection conn = DBConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement("SELECT log.logId, log.username, log.leagueId, l.leagueName, ml.team1_prediction, ml.team2_prediction, m.date, m.time, (SELECT t.teamName FROM team t where t.teamId = m.team1 ) AS team1, (SELECT t.teamName FROM team t where t.teamId = m.team2 ) AS team2 FROM log, matcheslog ml, league l, `match` as m WHERE log.logId = ml.logId and l.leagueId = log.leagueId and m.matchId = ml.matchId and log.username = ? limit 1");) {
+        try (Connection conn = DBConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement("SELECT log.logId, log.username, log.leagueId, l.leagueName, ml.team1_prediction, ml.team2_prediction, m.date, m.time, (SELECT t.teamName FROM team t where t.teamId = m.team1 ) AS team1, (SELECT t.teamName FROM team t where t.teamId = m.team2 ) AS team2 FROM log, matcheslog ml, league l, `match` as m WHERE log.logId = ml.logId and l.leagueId = log.leagueId and m.matchId = ml.matchId and log.username = ?");) {
              stmt.setString(1, username);
-            ResultSet rs = stmt.executeQuery();
+             ResultSet rs = stmt.executeQuery();
             
 
             while (rs.next()) {
@@ -39,11 +43,11 @@ public class PublicLeagueProfileDAO {
                 String team1 = rs.getString(9);
                 String team2 = rs.getString(10);
 
-                plProfile = new PublicLeagueProfile(logID, logUsername, leagueID, leagueName, team1Prediction, team2Prediction, matchDate, matchTime, team1, team2);
+                leagueList.add (new PublicLeagueProfile(logID, logUsername, leagueID, leagueName, team1Prediction, team2Prediction, matchDate, matchTime, team1, team2));
 
             }
             rs.close();
-            return plProfile;
+            return leagueList;
 
         } catch (SQLException ex) {
             ex.printStackTrace();
@@ -52,5 +56,36 @@ public class PublicLeagueProfileDAO {
         }
         return null;
     }
+public static User getUserInfo(String username) {
+    User u = new User();
+     try (Connection conn = DBConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement("Select * from user where username = ?");) {
+             stmt.setString(1, username);
+            ResultSet rs = stmt.executeQuery();
+            
 
+            while (rs.next()) {
+                String username1 = rs.getString(1);
+                String name = rs.getString(2);
+                String password = rs.getString(3);
+                Date dob  = rs.getDate(4);
+                String country = rs.getString(5);
+                String contactNum = rs.getString(6);
+                String email = rs.getString(7);
+                String favoriteTeam = rs.getString(8);
+
+
+                u = new User(username1, name,password, dob, country, contactNum, email, favoriteTeam);
+
+            }
+            rs.close();
+            return u;
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        
+    return null;
+}
 }
