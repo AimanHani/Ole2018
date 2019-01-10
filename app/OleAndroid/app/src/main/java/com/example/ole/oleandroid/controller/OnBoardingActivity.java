63f3dc96f05976ddf40bde1763721cca78ed647a
Liao Yi Ting <liaoyitingg@hotmail.com>
2019-01-10 18:58:32 +0800

    - edited and standardized buttons for each pages - touch up sign up page - changed task bar colors

--- app/OleAndroid/app/src/main/java/com/example/ole/oleandroid/controller/OnBoardingActivity.java
+++ app/OleAndroid/app/src/main/java/com/example/ole/oleandroid/controller/OnBoardingActivity.java
@@ -1,6 +1,8 @@
 package com.example.ole.oleandroid.controller;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
@@ -52,6 +54,12 @@ public class OnBoardingActivity extends AppCompatActivity {
         pager_indicator = (LinearLayout) findViewById(R.id.viewPagerCountDots);
 
         loadData();
+
+        // Making notification bar transparent
+        //if (Build.VERSION.SDK_INT >= 21) {
+        //    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        //    getWindow().setStatusBarColor(Color.TRANSPARENT);
+        //}
 
         mAdapter = new OnBoard_Adapter(this,onBoardItems);
         onboard_pager.setAdapter(mAdapter);
