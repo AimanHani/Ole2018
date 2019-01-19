b6835938bc786d7804d8caa08810278b9f9ac501
Nur Hazirah Haris <nurhazirahharis@gmail.com>
2019-01-20 01:15:09 +0800

    edited android login, tried to link to public league.
    Signed-off-by: Nur Hazirah Haris <nurhazirahharis@gmail.com>

---
+++ app/Ole WebService/WebService/src/java/model/PublicLeague.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author user
+ */
+public class PublicLeague {
+    private int leagueID;
+    private String prize;
+    private int tournamentID;
+    private int pointsAllocated;
+    private String tournamentName;
+
+    public String getTournamentName() {
+        return tournamentName;
+    }
+
+    public void setTournamentName(String tournamentName) {
+        this.tournamentName = tournamentName;
+    }
+
+    public PublicLeague(int leagueID) {
+        this.leagueID = leagueID;
+    }
+    private String leagueName;
+    
+
+ public PublicLeague(int leagueID, String prize, int tournamentID, int pointsAllocated, String leagueName) {
+        this.leagueID = leagueID;
+        this.prize = prize;
+        this.tournamentID = tournamentID;
+        this.pointsAllocated = pointsAllocated;
+        this.leagueName = leagueName;
+    }
+
+    public PublicLeague(int leagueID, String prize, int tournamentID, int pointsAllocated, String leagueName, String tournamentName) {
+        this.leagueID = leagueID;
+        this.prize = prize;
+        this.tournamentID = tournamentID;
+        this.pointsAllocated = pointsAllocated;
+        this.leagueName = leagueName;
+        this.tournamentName = tournamentName;
+    }
+    
+    public int getTournamentID() {
+        return tournamentID;
+    }
+
+    public void setTournamentID(int tournamentID) {
+        this.tournamentID = tournamentID;
+    }
+
+    public int getPointsAllocated() {
+        return pointsAllocated;
+    }
+
+    public void setPointsAllocated(int pointsAllocated) {
+        this.pointsAllocated = pointsAllocated;
+    }
+
+    public String getLeagueName() {
+        return leagueName;
+    }
+
+    public void setLeagueName(String leagueName) {
+        this.leagueName = leagueName;
+    }
+
+    public int getLeagueID() {
+        return leagueID;
+    }
+
+    public void setLeagueID(int leagueID) {
+        this.leagueID = leagueID;
+    }
+
+    public String getPrize() {
+        return prize;
+    }
+
+    public void setPrize(String prize) {
+        this.prize = prize;
+    }
+    
+}
