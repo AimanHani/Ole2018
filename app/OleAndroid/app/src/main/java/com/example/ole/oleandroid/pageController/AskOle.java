92a9fdfb402aefba2275840d608d6bc466ff1ea1
AimanHani <haniaimanbmi@gmail.com>
2019-01-25 15:13:07 +0800

    Cosmetic changes to Ask Ole and Leaderboard
    Signed-off-by: AimanHani <haniaimanbmi@gmail.com>

--- app/OleAndroid/app/src/main/java/com/example/ole/oleandroid/pageController/AskOle.java
+++ app/OleAndroid/app/src/main/java/com/example/ole/oleandroid/pageController/AskOle.java
@@ -3,6 +3,8 @@ package com.example.ole.oleandroid.pageController;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.ole.oleandroid.R;
+
 public class AskOle extends AppCompatActivity {
 
     @Override
