74a82812d3fcf071b902bb701426edfb32777423
Li Wei En <weien.li.2016@smu.edu.sg>
2019-01-25 13:15:04 +0800

    added method in GetMatchesDAO to retrieve past matches

--- app/Ole WebService/WebService/src/java/controller/GetMatchesDAO.java
+++ app/Ole WebService/WebService/src/java/controller/GetMatchesDAO.java
@@ -63,7 +63,7 @@ public class GetMatchesDAO {
                 Time matchTime = rs.getTime(3);
                 String team1 = rs.getString(4);
                 String team2 = rs.getString(5);
-                m = new Match(matchID,matchDate,matchTime,team1,team2);
+                m = new Match(matchID, matchDate, matchTime, team1, team2);
 
             }
             rs.close();
@@ -74,5 +74,34 @@ public class GetMatchesDAO {
             ex.printStackTrace();
         }
         return m;
+    }
+
+    public static HashMap<Integer, Match> getPastMatches() {
+        HashMap<Integer, Match> pastMatcehs = new HashMap<Integer, Match>();
+
+        try (Connection conn = DBConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement("select m.matchId,m.tournamentId, m.date, m.time, (SELECT t.teamName FROM team t where t.teamId = m.team1 ) AS team1, (SELECT t.teamName FROM team t where t.teamId = m.team2 ) AS team2, m.team1_score,m.team2_score from `match` m where date < DATE(NOW())  order by  m.date desc limit 6;");) {
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                int matchID = rs.getInt(1);
+                int tournamentID = rs.getInt(2);
+                Date matchDate = rs.getDate(3);
+                Time matchTime = rs.getTime(4);
+                String team1 = rs.getString(5);
+                String team2 = rs.getString(6);
+                int team1Score = rs.getInt(7);
+                int team2Score = rs.getInt(8);
+
+                pastMatcehs.put(matchID, new Match(matchID, tournamentID, matchDate, matchTime, team1, team2, team1Score, team2Score));
+
+            }
+            rs.close();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        return pastMatcehs;
     }
 }
