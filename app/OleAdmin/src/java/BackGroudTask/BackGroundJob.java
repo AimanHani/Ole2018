d946279abbe40d7d2cf5afae047435e6d5988313
Li Wei En <weien.li.2016@smu.edu.sg>
2019-01-29 13:33:12 +0800

    added method and package to implement schduletask

---
+++ app/OleAdmin/src/java/BackGroudTask/BackGroundJob.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BackGroudTask;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Web application lifecycle listener.
+ *
+ * @author user
+ */
+@WebListener
+public class BackGroundJob implements ServletContextListener {
+
+    private ScheduledExecutorService scheduler;
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        scheduler = Executors.newSingleThreadScheduledExecutor();
+//        scheduler.scheduleAtFixedRate(new SomeDailyJob(), 0, 1, TimeUnit.DAYS);
+//        scheduler.scheduleAtFixedRate(new SomeHourlyJob(), 0, 1, TimeUnit.HOURS);
+        try {
+            scheduler.scheduleAtFixedRate(new UpdateMatchTableByTimer(), 0, 1, TimeUnit.DAYS);
+        } catch (Exception e) {
+            e.printStackTrace(); 
+
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        scheduler.shutdownNow();
+    }
+}
