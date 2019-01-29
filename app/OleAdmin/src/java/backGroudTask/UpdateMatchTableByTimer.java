ee24e4b5f5d4b57bf8a097e2028c856814164eeb
Li Wei En <weien.li.2016@smu.edu.sg>
2019-01-29 13:38:46 +0800

    renamed the package name

--- app/OleAdmin/src/java/backGroudTask/UpdateMatchTableByTimer.java
+++ app/OleAdmin/src/java/backGroudTask/UpdateMatchTableByTimer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package BackGroudTask;
+package backGroudTask;
 
 import controller.APIDAO;
 import java.util.Iterator;
