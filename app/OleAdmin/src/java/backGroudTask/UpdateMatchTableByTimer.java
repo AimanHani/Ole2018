58b847151a06edbdf03608c6cbd3176ddcf49a36
Li Wei En <weien.li.2016@smu.edu.sg>
2019-02-02 16:42:19 +0800

    Change prepared statement to batch execute which boost up update process

--- app/OleAdmin/src/java/backGroudTask/UpdateMatchTableByTimer.java
+++ app/OleAdmin/src/java/backGroudTask/UpdateMatchTableByTimer.java
@@ -6,6 +6,10 @@
 package backGroudTask;
 
 import controller.APIDAO;
+import dbConnection.DBConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -38,49 +42,69 @@ public class UpdateMatchTableByTimer implements Runnable {
             jb1 = jb.getJSONObject("api");
             JSONObject jb2 = null;
             JSONObject jb3 = null;
-            int status = 0;
-            JSONObject finalResult = new JSONObject();
+
             jb3 = jb1.getJSONObject("fixtures");
             Iterator it = jb3.keys();
 
-            while (it.hasNext()) {
-                String keyStr = (String) it.next();
-                Object keyvalue = jb3.get(keyStr);
-                JSONObject eachMatch = (JSONObject) jb3.get(keyStr);
-                String leagueID = (String) eachMatch.get("league_id");
-                String strDate = (String) eachMatch.get("event_date");
-                String eTime = (String) eachMatch.get("event_timestamp");
-                String team1 = (String) eachMatch.get("homeTeam");
-                String team2 = (String) eachMatch.get("awayTeam");
-                String team1_score;
-                String team2_score;
-                if (eachMatch.get("goalsHomeTeam").equals(null)) {
-                    team1_score = null;
-                } else {
-                    team1_score = (String) eachMatch.get("goalsHomeTeam");
+            String team1_score = null;
+            String team2_score = null;
+            String id = null;
+            String statement = "update `match` set team1_score = '" + team1_score + "', team2_score = '" + team2_score + "' where matchId = " + id;
+            try (Connection c = DBConnection.getConnection(); PreparedStatement ps = c.prepareStatement(statement);) {
+
+                while (it.hasNext()) {
+                    String keyStr = (String) it.next();
+                    Object keyvalue = jb3.get(keyStr);
+                    JSONObject eachMatch = (JSONObject) jb3.get(keyStr);
+                    String leagueID = (String) eachMatch.get("league_id");
+                    String strDate = (String) eachMatch.get("event_date");
+                    String eTime = (String) eachMatch.get("event_timestamp");
+                    String team1 = (String) eachMatch.get("homeTeam");
+                    String team2 = (String) eachMatch.get("awayTeam");
+                    id = keyStr;
+                    if (eachMatch.get("goalsHomeTeam").equals(null)) {
+                        team1_score = null;
+                    } else {
+                        team1_score = (String) eachMatch.get("goalsHomeTeam");
+                    }
+
+                    if (eachMatch.get("goalsAwayTeam").equals(null)) {
+                        team2_score = null;
+                    } else {
+                        team2_score = (String) eachMatch.get("goalsAwayTeam");
+                    }
+
+                    if (team1_score == null) {
+                        team1_score = "-1";
+                    }
+                    if (team2_score == null) {
+                        team2_score = "-1";
+                    }
+
+                    statement = "update `match` set team1_score = '" + team1_score + "', team2_score = '" + team2_score + "' where matchId = " + id;
+                    //ps.setString(1, keyStr);
+                    //ps.setString(2, team2_score);
+                    //ps.setString(3, keyStr);
+
+                    String sql = ps.toString();
+                    ps.addBatch(statement);
+
+                    it.remove(); // avoids a ConcurrentModificationException
                 }
+                System.out.println("executing");
+                ps.executeBatch();
 
-                if (eachMatch.get("goalsAwayTeam").equals(null)) {
-                    team2_score = null;
-                } else {
-                    team2_score = (String) eachMatch.get("goalsAwayTeam");
-                }
+            } catch (SQLException ex) {
+                Logger.getLogger(APIDAO.class.getName()).log(Level.SEVERE, null, ex);
+                System.out.println(ex.getMessage());
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(APIDAO.class.getName()).log(Level.SEVERE, null, ex);
 
-                status += APIDAO.loadMatchesFromAPI(keyStr, leagueID, strDate, eTime, team1, team2, team1_score, team2_score);
-
-                it.remove(); // avoids a ConcurrentModificationException
-
-            }
-            if (status > 0) {
-                result.put("status", "error");
-            } else {
-                result.put("status", "successful");
-                System.out.println("update testload match table successfully");
             }
 
         } catch (JSONException ex) {
             Logger.getLogger(APICallServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+        System.out.println("execute finished");
     }
 }
