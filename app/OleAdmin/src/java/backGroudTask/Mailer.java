c9fd8e9287bdb698f95ae471e365c21d1ff9e3c7
Li Wei En <weien.li.2016@smu.edu.sg>
2019-04-05 13:01:56 +0800

    updated mailer class to notify user when the private league is deleted

--- app/OleAdmin/src/java/backGroudTask/Mailer.java
+++ app/OleAdmin/src/java/backGroudTask/Mailer.java
@@ -4,49 +4,52 @@
  * and open the template in the editor.
  */
 package backGroudTask;
-import java.util.Properties;    
-import javax.mail.*;    
-import javax.mail.internet.*;    
+
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+
 /**
  *
  * @author user
  */
 public class Mailer {
-    public static boolean send(String from,String password,String to,String sub,String msg){  
-        
-          boolean status = false;
-         Properties props = new Properties();
+
+    public static boolean send(String from, String password, String to, String sub, String msg) {
+
+        boolean status = false;
+        Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
- 
-         Session session = Session.getInstance(props,
+
+        Session session = Session.getInstance(props,
                 new javax.mail.Authenticator() {
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(from, password);
-                    }
-                });
-          
-         
-          try {
- 
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, password);
+            }
+        });
+
+        try {
+
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress(from));
             message.setRecipients(Message.RecipientType.TO,
                     InternetAddress.parse(to));
             message.setSubject(sub);
             message.setText(msg);
- 
+
             Transport.send(message);
- 
+
             System.out.println("Mail sent succesfully!");
             status = true;
- 
+
         } catch (MessagingException e) {
             throw new RuntimeException(e);
         }
-          
-           return status;
+
+        return status;
     }
-}  
+
+}
