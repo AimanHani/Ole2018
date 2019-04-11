980ed749d8feeee795070b36b349759595878cdc
Nurfarihin Kamaruzzaman <nurfarihink.2016@sis.smu.edu.sg>
2019-04-11 12:27:31 +0800

    deleted unused files

--- app/OleAndroid/app/src/main/java/com/example/ole/oleandroid/controller/PrivateLeagueSpecialsList.java
+++
@@ -1,4 +0,0 @@
-package com.example.ole.oleandroid.controller;
-
-public class PrivateLeagueSpecialsList {
-}
--- app/OleAndroid/app/src/main/java/com/example/ole/oleandroid/pageController/Page.java
+++
@@ -1,6 +0,0 @@
-package com.example.ole.oleandroid.pageController;
-
-public class Page {
-
-    // this is a test page to load page controller package.
-}
